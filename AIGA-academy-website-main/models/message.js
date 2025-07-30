735a4a72e2e3efe4ef6e71c1e7fbd887e8dc6456
Aligaziz <aligaziznu@gmail.com>
2025-07-31 02:20:43 +0600

    Delete AIGA-academy-website-main/models directory

--- AIGA-academy-website-main/models/message.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Message = sequelize.define('Message', {
-    from: DataTypes.INTEGER,
-    to: DataTypes.INTEGER,
-    text: DataTypes.TEXT
-  });
-
-  return Message;
-};
