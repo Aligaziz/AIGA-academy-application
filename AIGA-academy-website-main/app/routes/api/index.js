704febda0103a021d3450febd62cf562ac44188b
Aligaziz <aligaziznu@gmail.com>
2025-07-31 19:18:20 +0600

    Update index.js

--- AIGA-academy-website-main/app/routes/api/index.js
+++ AIGA-academy-website-main/app/routes/api/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 router.use('/bank', bankRoutes);
 router.use('/chat', chatRoutes);
+const lessonRoutes = require('./lessonRoutes');
+const clientRoutes = require('./clientRoutes');
+router.use('/lesson', lessonRoutes);
+router.use('/client', clientRoutes);
 module.exports = versionString => ({
   private: require(`./${versionString}/privateRoutes`),
   public: require(`./${versionString}/publicRoutes`),
