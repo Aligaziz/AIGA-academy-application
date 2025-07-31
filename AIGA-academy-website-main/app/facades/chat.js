f63bcd9fc2630285b213c8d2862f96a0f18bc462
Aligaziz <aligaziznu@gmail.com>
2025-07-31 16:44:34 +0600

    Update chat.js

--- AIGA-academy-website-main/app/facades/chat.js
+++ AIGA-academy-website-main/app/facades/chat.js
@@ -24,6 +24,9 @@ async function getDialog({ userId, otherUserId }) {
         { fromUserId: otherUserId, toUserId: userId },
       ],
     },
+    // In Message.create, validate that fromUserId and toUserId have a valid coach-client relationship
+    const isValidRelation = await CoachClient.findOne({ where: { coachId: fromUserId, clientId: toUserId } });
+    if (!isValidRelation) throw new Err('Invalid recipient for this role', 403);
     order: [['createdAt', 'ASC']],
   });
 
